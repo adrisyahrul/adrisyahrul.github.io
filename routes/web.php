620a1ee33366ce8dcc4429ade54ba5652bbc8646
Adri Syahrul <adrydhelfa20@gmail.com>
2021-12-21 15:36:29 +0700

    tes

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('front/index');
+    return view('front/pages/index');
 });
 
 Route::get('hallo', function () {
