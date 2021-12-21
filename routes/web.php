6d5fbf98e5cb0d7b0db69fb7e653cfb11858f874
Adri Syahrul <adrydhelfa20@gmail.com>
2021-12-21 14:35:34 +0700

    Moving to Laravel project

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('front/index');
+});
+
+Route::get('hallo', function () {
+    return "hallo, lorem ipsum";
 });
